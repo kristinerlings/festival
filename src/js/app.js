ca6d267e256aa554bf89d7d636d5e535c7016c69
kristinerlings <kristin.erlingsdottir@student.howest.be>
2023-07-01 19:30:50 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import '../css/style.css';
+
+// Accept HMR as per: https://vitejs.dev/guide/api-hmr.html
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    console.log('HMR');
+  });
+}
